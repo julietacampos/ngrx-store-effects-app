0c85bdd62e4bb4fc633f0711f38bfa5427f829c6
Julieta Campos <julieta.guzman@etventure.com>
2018-02-24 14:05:10 +0100

    Update with ngrx

--- src/products/store/actions/toppings.action.ts
+++ src/products/store/actions/toppings.action.ts
@@ -29,4 +29,8 @@ export class VisualiseToppings implements Action {
 
 // action types
 
-export type ToppingsAction = | LoadToppings | LoadToppingsFail | LoadToppingsSuccess | VisualiseToppings;
+export type ToppingsAction =
+| LoadToppings
+| LoadToppingsFail
+| LoadToppingsSuccess
+| VisualiseToppings;
